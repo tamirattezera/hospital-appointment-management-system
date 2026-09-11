b9f590f3c93f0edd093ccaa95f92f6552ba2cbf2
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-11 16:23:46 +0300

    feat: add abstract person model

---
+++ src/hospital/model/Person.java
@@ -0,0 +1,44 @@
+package hospital.model;
+
+public abstract class Person {
+
+    private String id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public Person(String id, String name, String phone, String email) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public abstract void displayRoleInformation();
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", phone='" + phone + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
