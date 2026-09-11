e3dbd38444777209af497069a0534da8af26ccd5
Tamirat Tezera <tamirattezera8@gmail.com>
2026-09-11 18:33:32 +0300

    feat: add invalid appointment exception

---
+++ src/hospital/exception/InvalidAppointmentException.java
@@ -0,0 +1,8 @@
+package hospital.exception;
+
+public class InvalidAppointmentException extends Exception {
+
+    public InvalidAppointmentException(String message) {
+        super(message);
+    }
+}
